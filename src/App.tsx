f20bb8a3a5eea65357914ef055bfda204a883e94
Ezequiel Barbero <ezequiel.barba94@gmail.com>
2021-05-07 23:12:49 -0300

    Adds styling to SearchBar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import {SearchBar} from './delivery/SearchBar'
 
 const App: React.FC = () =>
-    <div className="container">
+    <div className="container pt-4">
+        <h1>News Search</h1>
         <SearchBar search={value => console.log(value)}/>
     </div>
 
--- src/delivery/SearchBar.tsx
+++ src/delivery/SearchBar.tsx
@@ -7,8 +7,9 @@ interface SearchBarProps {
 
 export const SearchBar: React.FC<SearchBarProps> = ({search}) =>
     <Formik initialValues={{search: ''}} onSubmit={values => search(values.search)}>
-        <Form>
-            <Field name="search" type="text"/>
-            <button type="submit">Search</button>
+        <Form className="form-inline">
+            <label htmlFor="search" className="sr-only">Search</label>
+            <Field className="form-control flex-grow-1 mr-sm-2" id="search" name="search" type="text"/>
+            <button className="btn btn-primary px-4 my-2 flex-fill flex-sm-grow-0" type="submit">Search</button>
         </Form>
     </Formik>
